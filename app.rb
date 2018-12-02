a37e114a226018e0ae528fa3835e0871138365f8
tracydaniellelong <tracydaniellelong@gmail.com>
2018-12-02 18:56:01 -0500

    working app just need to add styling

--- app.rb
+++ app.rb
@@ -1,2 +1,17 @@
 require 'sinatra'
+require_relative 'coinchanger'
 
+get '/' do
+  erb :home
+end
+
+post '/home' do
+  coins = params[:cents]
+  redirect '/coins?cents=' + coins
+end
+
+get '/coins' do
+  cents = params[:cents].to_i
+  coins = change(cents)
+  erb :coins, locals: {cents: cents, coins: coins}
+end
--- coinchanger.rb
+++ coinchanger.rb
@@ -1,20 +1,16 @@
 def change(cents)
 	coins = {
-		:quarter => 25, 
+		:quarter => 25,
 		:dime => 10,
 		:nickel => 5,
 		:penny => 1
 	}
-	if cents.class != Integer
-		return "Enter only Integers."
-	end
-
 	change = {}
 	coins.each do |coin, value|
 		if (cents / value) >= 1
 			change[coin] = cents / value
 			cents %= value
-		end	
+		end
 	end
 	return change
 end
